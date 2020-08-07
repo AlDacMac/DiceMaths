4a7e2047b6856eebf5060b569ba7a05beb190b54
AlDacMac <alasdairmacgsign@gmail.com>
2020-08-08 00:36:03 +0100

    comment out print statements

--- dicemaths/infinity.py
+++ dicemaths/infinity.py
@@ -10,18 +10,18 @@ def contested_roll_hit_avg(attacker_burst, attacker_target, target_burst, target
     single_dice_crit_prob = 0.0
     for i in range(1, 21):
         value_rolled = i + attacker_bonus
-        print("_rolled {0}".format(value_rolled))
+        #print("_rolled {0}".format(value_rolled))
         if(value_rolled == attacker_target or value_rolled > 20):
             prob_no_contest = prob_none(target_burst, 1 + target_bonus, 20)
             prob_crit_from_roll = (1.0 / 20) * prob_no_contest
             single_dice_crit_prob += prob_crit_from_roll
-            print("crit probability: {0}".format(prob_crit_from_roll))
+            #print("crit probability: {0}".format(prob_crit_from_roll))
         elif(value_rolled < attacker_target):
             window_with_mods = min(20, (target_target - value_rolled + 1 + target_bonus))
             prob_no_contest = prob_none(target_burst, window_with_mods, 20)
             prob_hit_from_roll = (1.0 / 20) * prob_no_contest
             single_dice_hit_prob += prob_hit_from_roll
-            print(prob_hit_from_roll)
+            #print(prob_hit_from_roll)
     return (attacker_burst * single_dice_hit_prob, attacker_burst * single_dice_crit_prob)
 
 
@@ -29,10 +29,10 @@ def contested_roll_crit_avg(attacker_burst, attacker_target, attacker_bonus, tar
     singleDiceProb = 0.0
     for i in range(1, 21):
         value_rolled = i + attacker_bonus
-        print("_rolled {0}".format(value_rolled))
+        #print("_rolled {0}".format(value_rolled))
         if (value_rolled == attacker_target or value_rolled > 20):
             prob_no_contest = prob_none(target_burst, 1 + target_bonus, 20)
             prob_hit_from_roll = (1.0 / 20) * prob_no_contest
             singleDiceProb += prob_hit_from_roll
-            print(prob_hit_from_roll)
+            #print(prob_hit_from_roll)
     return attacker_burst * singleDiceProb
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dicemaths",
-    version="0.1.1",
+    version="0.1.2",
     author="AlDacMac",
     author_email="alasdairmacgdev@gmail.com",
     description="Package for performing basic statistical calculations on the outcomes of dice rolls",
