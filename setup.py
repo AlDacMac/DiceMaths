039302cdd67344d2743ebf511819961c9a291067
AlDacMac <alasdairmacgsign@gmail.com>
2020-09-03 16:14:23 +0100

    Set version number to 2.0
    The feature change is that now I'm actually going to use decent version
    numbers.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dicemaths",
-    version="0.1.3",
+    version="0.2.0",
     author="AlDacMac",
     author_email="alasdairmacgdev@gmail.com",
     description="Package for performing basic statistical calculations on the outcomes of dice rolls",
